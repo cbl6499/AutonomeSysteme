62a9133c812c6154194ad404d2882305af4514d6
janflei <fleisch.ja@gmail.com>
2018-11-13 10:13:36 +0100

    setup

--- Controller/parkRobot.py
+++ Controller/parkRobot.py
@@ -1,29 +1,33 @@
 import time
 from BasicEPuck.ePuckVRep import EPuckVRep
+import numpy as np
 
 # lists with behavior constants
 # obstacles in front of the robot
-braitFrontSens_leftMotor = [1, 2, 0, 0]
-braitFrontSens_rightMotor = [-1, -2, 0, 0]
+#braitFrontSens_leftMotor = [1, 2, 0, 0]
+#braitFrontSens_rightMotor = [-1, -2, 0, 0]
 
 # obstacles at the side of the robot
-braitSideSens_leftMotor = [-1, 0]
-braitSideSens_rightMotor = [0, -1]
+#braitSideSens_leftMotor = [-1, 0]
+#braitSideSens_rightMotor = [0, -1]
 
 
-def calc_distance(distances, noDetectionDistance):
-    distances = 1 - (distances / noDetectionDistance)
-    return distances
+#def calc_distance(distances, noDetectionDistance):
+#    distances = 1 - (distances / noDetectionDistance)
+#    return distances
 
 
-def calculateMotorValues(distances, noDetectionDistance):
-    dist = calc_distance(distances, noDetectionDistance)
+def calculateMotorValues(current_pose, noDetectionDistance):
+    delta_x = 1
+
+    roh = np.sqrt(delta_x)
     maxVel = 120 * 3.1415 / 180  # 4/3 of a full wheel turn
 
-    velRight = maxVel - (dist[3] + dist[4] + dist[5])
-    velLeft = maxVel - (dist[0] + dist[1] + dist[2])
+    #velRight = maxVel - (dist[3] + dist[4] + dist[5])
+    #velLeft = maxVel - (dist[0] + dist[1] + dist[2])
 
-    return velLeft, velRight
+
+    return 1
 
 
 def main():
@@ -32,7 +36,7 @@ def main():
     robot.enableAllSensors()
     robot.setSensesAllTogether(True)   # we want fast sensing, so set robot to sensing mode where all sensors are sensed
 
-    noDetectionDistance = 0.05 * robot.getS()  # maximum distance that proximity sensors of ePuck may sense
+    #noDetectionDistance = 0.05 * robot.getS()  # maximum distance that proximity sensors of ePuck may sense
 
     # main sense-act cycle
     while robot.isConnected():
@@ -40,7 +44,9 @@ def main():
         robot.fastSensingOverSignal()
 
         distVector = robot.getProximitySensorValues()
-        leftMotor, rightMotor = calculateMotorValues(distVector, noDetectionDistance)
+        current_pose = robot._getPose()
+        print current_pose
+        #leftMotor, rightMotor = calculateMotorValues(current_pose)
 
         robot.setMotorSpeeds(leftMotor, rightMotor)
 
