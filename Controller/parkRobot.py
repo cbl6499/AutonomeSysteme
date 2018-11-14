5f2d80c924cc7d637697ced45c4dd35d7710a361
janflei <fleisch.ja@gmail.com>
2018-11-14 07:56:12 +0100

    stop in front of the cube added scene for printing route more speed during moving from curren to final

--- Controller/parkRobot.py
+++ Controller/parkRobot.py
@@ -2,27 +2,16 @@ import time
 from BasicEPuck.ePuckVRep import EPuckVRep
 import numpy as np
 
-# lists with behavior constants
-# obstacles in front of the robot
-#braitFrontSens_leftMotor = [1, 2, 0, 0]
-#braitFrontSens_rightMotor = [-1, -2, 0, 0]
-
-# obstacles at the side of the robot
-#braitSideSens_leftMotor = [-1, 0]
-#braitSideSens_rightMotor = [0, -1]
 
 
-#def calc_distance(distances, noDetectionDistance):
-#    distances = 1 - (distances / noDetectionDistance)
-#    return distances
-
-
-
-
-def calculateMotorValues(current_pose, final_pose, matrix_k, wheel_radius, wheel_distance):
+def calculateMotorValues(current_pose, final_pose, wheel_radius, wheel_distance):
     delta_x = final_pose[0] - current_pose[0]
     delta_y = final_pose[1] - current_pose[1]
 
+    matrix_k = np.array([
+        [0.04, 0, 0],
+        [0, 0.2, -0.1]
+    ])
 
     roh = np.sqrt((delta_x**2) + (delta_y**2))
     alpha = -(current_pose[2]) + np.arctan2(delta_y, delta_x)
@@ -31,53 +20,45 @@ def calculateMotorValues(current_pose, final_pose, matrix_k, wheel_radius, wheel
     phi_r = (matrix_k[0,0] / wheel_radius * roh) + ((matrix_k[1,1]*wheel_distance) / wheel_radius * alpha) + ((matrix_k[1,2]*wheel_distance) / wheel_radius * beta)
     phi_l = (matrix_k[0,0] / wheel_radius * roh) - ((matrix_k[1,1]*wheel_distance) / wheel_radius * alpha) - ((matrix_k[1,2]*wheel_distance) / wheel_radius * beta)
 
-
-
-    #maxVel = 120 * 3.1415 / 180  # 4/3 of a full wheel turn
-
-    #velRight = maxVel - (dist[3] + dist[4] + dist[5])
-    #velLeft = maxVel - (dist[0] + dist[1] + dist[2])
-
-
     return phi_l, phi_r
 
 
 def main():
+
     final_pose = np.array([
                         [1.225],
                         [0.0],
                         [0.0]])
 
-    matrix_k = np.array([
-            [0.03, 0, 0],
-            [0, 0.2, -0.1]
-        ])
-
     robot = EPuckVRep('ePuck', port=19999, synchronous=False)
 
-    wheel_radius = robot._wheelDiameter / 2;
+    wheel_radius = robot._wheelDiameter / 2
 
     wheel_distance = robot._wheelDistance
 
     robot.enablePose()
 
-    #noDetectionDistance = 0.05 * robot.getS()  # maximum distance that proximity sensors of ePuck may sense
-
     # main sense-act cycle
     while robot.isConnected():
 
-        #robot.fastSensingOverSignal()
-        #distVector = robot.getProximitySensorValues()
-
         current_pose = robot._getPose()
-        print wheel_radius
-        leftMotor, rightMotor = calculateMotorValues(current_pose, final_pose, matrix_k, wheel_radius, wheel_distance)
 
-        robot.setMotorSpeeds(leftMotor, rightMotor)
+        leftMotor, rightMotor = calculateMotorValues(current_pose, final_pose, wheel_radius, wheel_distance)
 
-        #maxVel = 120 * 3.1415 / 180
-        #robot.setMotorSpeeds(maxVel, maxVel)
+        #print leftMotor, rightMotor
 
+        if leftMotor < 1.5 and rightMotor < 1.5:
+            leftMotor = leftMotor + 0.7
+            rightMotor = rightMotor + 0.7
+
+        if round(current_pose[0],1) == round(final_pose[0],1) and round(current_pose[1],1) == final_pose[1] and round(current_pose[2],1) == final_pose[2]:
+
+            robot.setMotorSpeeds(0, 0)
+            print "STOP"
+
+        else:
+            print "current_pose", current_pose[0], current_pose[1], current_pose[2]
+            robot.setMotorSpeeds(leftMotor, rightMotor)
 
         time.sleep(0.05)
 
