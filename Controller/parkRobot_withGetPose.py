000332354cf357314ce2c320d26b9a75ebfffd9d
cbl6499 <clemens.blecha@students.fhv.at>
2018-11-15 16:27:45 +0100

    tuat ned

--- Controller/parkRobot_withGetPose.py
+++ Controller/parkRobot_withGetPose.py
@@ -3,7 +3,6 @@ from BasicEPuck.ePuckVRep import EPuckVRep
 import numpy as np
 
 
-
 def calculateMotorValues(current_pose, final_pose, wheel_radius, wheel_distance):
     delta_x = final_pose[0] - current_pose[0]
     delta_y = final_pose[1] - current_pose[1]
@@ -17,10 +16,21 @@ def calculateMotorValues(current_pose, final_pose, wheel_radius, wheel_distance)
     alpha = -(current_pose[2]) + np.arctan2(delta_y, delta_x)
     beta = - (current_pose[2]) - alpha
 
-    phi_r = (matrix_k[0,0] / wheel_radius * roh) + ((matrix_k[1,1]*wheel_distance) / wheel_radius * alpha) + ((matrix_k[1,2]*wheel_distance) / wheel_radius * beta)
-    phi_l = (matrix_k[0,0] / wheel_radius * roh) - ((matrix_k[1,1]*wheel_distance) / wheel_radius * alpha) - ((matrix_k[1,2]*wheel_distance) / wheel_radius * beta)
+    #phi_r = ((matrix_k[0,0] / wheel_radius) * roh) + (((matrix_k[1,1]*wheel_distance) / wheel_radius) * alpha) + (((matrix_k[1,2]*wheel_distance) / wheel_radius) * beta)
+    #phi_l = ((matrix_k[0,0] / wheel_radius) * roh) - (((matrix_k[1,1]*wheel_distance) / wheel_radius) * alpha) - (((matrix_k[1,2]*wheel_distance) / wheel_radius) * beta)
 
-    return phi_l, phi_r
+    phi_lr = np.array([matrix_k[0, 0] / wheel_radius, (matrix_k[1, 1]*wheel_distance) / wheel_radius, (matrix_k[1, 2]*wheel_distance) / wheel_radius],
+                      [matrix_k[0, 0] / wheel_radius, -(matrix_k[1, 1]*wheel_distance) / wheel_radius, -(matrix_k[1, 2]*wheel_distance) / wheel_radius])
+
+    angles = np.array([roh],
+                      [alpha],
+                      [beta])
+
+    phi_l_r = phi_lr * angles
+
+    print phi_l_r
+
+    return phi_l_r[0], phi_l_r[1]
 
 
 def main():
@@ -39,6 +49,8 @@ def main():
 
     robot.enableWheelEncoding()
 
+    found = False
+
     # main sense-act cycle
     while robot.isConnected():
 
@@ -56,20 +68,24 @@ def main():
 
 
         #if  0.05 < leftMotor < 1.5 and 0.05 < rightMotor < 1.5:
-        #    leftMotor = leftMotor + 0.7
-        #    rightMotor = rightMotor + 0.7
+        #   leftMotor = leftMotor + 1
+        #    rightMotor = rightMotor + 1
 
         tol = 0.02
 
-        if final_pose[0] - tol < current_pose[0] < final_pose[0] + tol and \
-           final_pose[1] - tol < current_pose[1] < final_pose[1] + tol and \
-           final_pose[2] - tol < current_pose[2] < final_pose[2] + tol:
-            robot.setMotorSpeeds(0, 0)
-            print "STOP"
+        if found != True:
+            if final_pose[0] - tol < current_pose[0] < final_pose[0] + tol and \
+                    final_pose[1] - tol < current_pose[1] < final_pose[1] + tol and \
+                    final_pose[2] - tol < current_pose[2] < final_pose[2] + tol:
+                robot.setMotorSpeeds(0, 0)
+                found = True
+                #print "STOP"
 
-        else:
-            print "current_pose", current_pose[0], current_pose[1], current_pose[2]
-            robot.setMotorSpeeds(leftMotor, rightMotor)
+            else:
+                print "current_pose", current_pose[0], current_pose[1], current_pose[2]
+                robot.setMotorSpeeds(leftMotor, rightMotor)
+
+        print found
 
         time.sleep(0.05)
 
